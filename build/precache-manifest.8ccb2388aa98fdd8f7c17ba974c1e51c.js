004ca71738921612734c389a1e785a0c7c3941a9
SoftDev-2019 <54074920+SoftDev-2019@users.noreply.github.com>
2019-11-18 14:40:33 -0600

    initial commit

---
+++ build/precache-manifest.8ccb2388aa98fdd8f7c17ba974c1e51c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a71df4cfab85df5370fc7e85de3a496",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2dee2120331feb8d4d7",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "174b43b2f7f5ad994bd8",
+    "url": "/static/css/main.b3e1f362.chunk.css"
+  },
+  {
+    "revision": "a2dee2120331feb8d4d7",
+    "url": "/static/js/2.c60dbcd6.chunk.js"
+  },
+  {
+    "revision": "174b43b2f7f5ad994bd8",
+    "url": "/static/js/main.294f88d5.chunk.js"
+  },
+  {
+    "revision": "d168dfd6372324ff0ba5",
+    "url": "/static/js/runtime-main.a2d52a5d.js"
+  },
+  {
+    "revision": "49df4fd8de857eee30f96d59825f0477",
+    "url": "/static/media/BrockBatStats10-11.49df4fd8.png"
+  },
+  {
+    "revision": "6bd5fa7e9158f9c37f7615e297ba7dad",
+    "url": "/static/media/BrockPitchStats10-11.6bd5fa7e.png"
+  },
+  {
+    "revision": "dc81a45dc2cdc50068cc828fb8fe8453",
+    "url": "/static/media/FallClassic.dc81a45d.PNG"
+  },
+  {
+    "revision": "cdeb52aa4c8e35ced21ec39d1436654d",
+    "url": "/static/media/Generals2.cdeb52aa.jpeg"
+  },
+  {
+    "revision": "15188fc0caf6021abd3b0826c0b6b217",
+    "url": "/static/media/GeneralsPracticeLogo.15188fc0.jpeg"
+  },
+  {
+    "revision": "94ff5e8dce51883b0b104ec222937aea",
+    "url": "/static/media/Generals_Game_Jersey_1.94ff5e8d.JPEG"
+  },
+  {
+    "revision": "3748a007399201dd9fc4046001bcbf9a",
+    "url": "/static/media/Generals_Hat_1.3748a007.JPEG"
+  },
+  {
+    "revision": "9d9f521e3efdf0012e7431cad08cc518",
+    "url": "/static/media/Generals_Practice_Jersey_1.9d9f521e.JPEG"
+  },
+  {
+    "revision": "da2e4c35009ecfaf86e913b5175122d1",
+    "url": "/static/media/ImgBrock.da2e4c35.jpeg"
+  },
+  {
+    "revision": "16083a4617aa379a3c6f5989aec7937c",
+    "url": "/static/media/crossedBats.16083a46.png"
+  },
+  {
+    "revision": "bc5c773d35837db817d2aaa003cfe644",
+    "url": "/static/media/generals1.bc5c773d.jpeg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "82b991de11e8acc61d2e794d5a3b4696",
+    "url": "/static/media/sponsorImage.82b991de.jpeg"
+  }
+]);
