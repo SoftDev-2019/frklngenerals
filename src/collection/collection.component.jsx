40e5bb7a78a8276e186de625aff4efa3ddab8177
SoftDev-2019 <54074920+SoftDev-2019@users.noreply.github.com>
2019-12-04 20:23:06 -0600

    upated css for collection component

--- src/collection/collection.component.jsx
+++ src/collection/collection.component.jsx
@@ -8,9 +8,9 @@ import CollectionItem from '../collection-items/collection-items.component';
 const CollectionPage = ({ collection }) => {
     const { title, items } = collection;
     return (
-    <div className='collection-page'>
-        <h2 className='title'>{ title }</h2>
-        <div className='items'>
+    <div className='collection-page-container'>
+        <h2 className='collection-page-container-title'>{ title }</h2>
+        <div className='collection-page-container-items'>
             {
                 items.map(item => (<CollectionItem key={item.id} item={item} />)
             )};
