712d3ee9a8f2862763bcce4c9d2f1837d7a1f358
SoftDev-2019 <54074920+SoftDev-2019@users.noreply.github.com>
2019-12-07 07:28:13 -0600

    updated firebase script files and build.

---
+++ build/precache-manifest.ae04cbc46c023aa7eef5e7b1d8cf81e1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721918e275336b9ae7d4ded834216646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60ef87760c0321a3b45a",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "61010b04f4a39e209b4d",
+    "url": "/static/css/main.81a32044.chunk.css"
+  },
+  {
+    "revision": "60ef87760c0321a3b45a",
+    "url": "/static/js/2.05654c21.chunk.js"
+  },
+  {
+    "revision": "61010b04f4a39e209b4d",
+    "url": "/static/js/main.e8d5e256.chunk.js"
+  },
+  {
+    "revision": "d168dfd6372324ff0ba5",
+    "url": "/static/js/runtime-main.a2d52a5d.js"
+  },
+  {
+    "revision": "49df4fd8de857eee30f96d59825f0477",
+    "url": "/static/media/BrockBatStats10-11.49df4fd8.png"
+  },
+  {
+    "revision": "6bd5fa7e9158f9c37f7615e297ba7dad",
+    "url": "/static/media/BrockPitchStats10-11.6bd5fa7e.png"
+  },
+  {
+    "revision": "dc81a45dc2cdc50068cc828fb8fe8453",
+    "url": "/static/media/FallClassic.dc81a45d.PNG"
+  },
+  {
+    "revision": "da2e4c35009ecfaf86e913b5175122d1",
+    "url": "/static/media/ImgBrock.da2e4c35.jpeg"
+  },
+  {
+    "revision": "30c6fedfb4a0118ca3b5a5c9f25111bf",
+    "url": "/static/media/Logo.30c6fedf.png"
+  },
+  {
+    "revision": "16083a4617aa379a3c6f5989aec7937c",
+    "url": "/static/media/crossedBats.16083a46.png"
+  },
+  {
+    "revision": "bc5c773d35837db817d2aaa003cfe644",
+    "url": "/static/media/generals1.bc5c773d.jpeg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "82b991de11e8acc61d2e794d5a3b4696",
+    "url": "/static/media/sponsorImage.82b991de.jpeg"
+  }
+]);
